b6d56c7052ca6e08c5420f3bfc3f9f257746c750
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 14:19:33 +0530

    Programme file

---
+++ SwappingNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograme
+{
+    internal class SwappingNumber
+    {
+        public static void solution()
+        {
+            Console.WriteLine("Enter A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int Value;
+            Value = a;
+            Console.WriteLine("Swapped Value of A= " + b);
+            Console.ReadLine();
+            Console.WriteLine("Swapped value of B= " + a);
+            Console.ReadLine();
+        }
+
+        }
+}
