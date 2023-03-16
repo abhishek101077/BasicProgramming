56169c9a0054ff9bce01fb6e946ba136aaa5c50b
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 15:10:42 +0530

    programme for Harmonic number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograme
+{
+    internal class HarmonicNumber
+    {
+        public static void solution()
+        {
+            int i, n;
+            double s = 0.0;
+
+
+            Console.WriteLine("Calculate the harmonic series and their sum:\n");
+
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} ", n, s);
+            Console.ReadLine();
+        }
+        }
+}
