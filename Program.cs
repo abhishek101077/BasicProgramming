4f06756c78b745d8cdb47f4d03821f58210663a9
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 14:14:09 +0530

    Programme file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicPrograme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Even_and_Odd.solution();
+            VowelsAndConsonent.solution();
+        }
+    }
+}
